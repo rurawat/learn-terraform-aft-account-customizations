b81aa5b3688246ff586515d6c885b651403da8c0
Ruchita Rawat <ruchita.rawat@cloudreach.com>
2022-08-09 16:28:15 +0200

    Create sandbox-aft Control Tower account

---
+++ vpctesting/terraform/locals.tf
@@ -0,0 +1,21 @@
+
+locals {
+  vpc-cidr = "192.168.0.0/24"
+  region   = "eu-west02"
+  subnet_cidr1_az1 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),0)
+  subnet_cidr1_az2 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),1)
+  subnet_cidr1_az3 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),2)
+  subnet_cidr2_az1 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),3)
+  subnet_cidr2_az2 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),4)
+  subnet_cidr2_az3 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),5)
+  subnet_cidr3_az1 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),6)
+  subnet_cidr3_az2 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),7)
+  subnet_cidr3_az3 = element(cidrsubnets("192.168.0.0/24", 3, 3, 3, 4,4,4,4,4,4),8)
+
+  CustomerName = "TestyBoi"
+  environment  = "Test"
+
+  #vpc-region = (local.region t== "eu-west-2" ? data.terraform_remote_state.networking_state.outputs.vpc-london : data.terraform_remote_state.networking_state.outputs.vpc-ohio)
+
+  account_id = "146922398226"
+}
---
+++ vpctesting/terraform/main.tf
@@ -0,0 +1,46 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  providers = {
+    aws = aws.region-selection
+  }
+  version = "3.11.3"
+
+
+  name = "vpc-${local.region}-${local.environment}"
+  cidr = local.vpc-cidr
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["${local.subnet_cidr3_az1}", "${local.subnet_cidr3_az2}", "${local.subnet_cidr3_az3}"]
+  private_subnets  = ["${local.subnet_cidr1_az1}", "${local.subnet_cidr1_az2}", "${local.subnet_cidr1_az3}"]
+  database_subnets = ["${local.subnet_cidr2_az1}", "${local.subnet_cidr2_az2}", "${local.subnet_cidr2_az3}"]
+
+  enable_vpn_gateway                 = false
+  propagate_private_route_tables_vgw = false
+  enable_nat_gateway                 = false
+  enable_dns_hostnames               = false
+
+  public_subnet_tags = {
+    Name = "net-${local.region}-${local.environment}-public-${local.CustomerName}"
+  }
+
+  private_subnet_tags = {
+    Name = "net-${local.region}-${local.environment}-application-${local.CustomerName}"
+  }
+
+  database_subnet_tags = {
+    Name = "net-${local.region}-${local.environment}-data-${local.CustomerName}"
+  }
+
+  private_route_table_tags = {
+    # Name = "rtb-${local.region}-${local.environment}-${local.purpose}"
+  }
+
+  tags = {
+    CustomerName = "${local.CustomerName}"
+    Environment  = "${local.environment}"
+  }
+
+  vpc_tags = {
+    Name = "vpc-${local.region}-${local.environment}"
+  }
+}
