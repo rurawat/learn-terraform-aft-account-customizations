c906e08d5ddf0801daa9bd4b6c51019b53421ed9
Ruchita Rawat <ruchita.rawat@cloudreach.com>
2022-08-11 10:08:25 +0200

    k

--- test/terraform/vpc.tf
+++ test/terraform/vpc.tf
@@ -10,9 +10,9 @@ module "vpc" {
   cidr = local.vpc-cidr
 
   azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets   = ["${local.subnet_cidr3_az1}", "${local.subnet_cidr3_az2}", "${local.subnet_cidr3_az3}"]
-  private_subnets  = ["${local.subnet_cidr1_az1}", "${local.subnet_cidr1_az2}", "${local.subnet_cidr1_az3}"]
-  database_subnets = ["${local.subnet_cidr2_az1}", "${local.subnet_cidr2_az2}", "${local.subnet_cidr2_az3}"]
+  public_subnets   = ["${local.subnet_cidr3_az1}", "${local.subnet_cidr3_az2}"]
+  #private_subnets  = ["${local.subnet_cidr1_az1}", "${local.subnet_cidr1_az2}", "${local.subnet_cidr1_az3}"]
+  #database_subnets = ["${local.subnet_cidr2_az1}", "${local.subnet_cidr2_az2}", "${local.subnet_cidr2_az3}"]
 
   enable_vpn_gateway                 = false
   propagate_private_route_tables_vgw = false
@@ -23,17 +23,17 @@ module "vpc" {
     Name = "net-${local.region}-${local.environment}-public-${local.CustomerName}"
   }
 
-  private_subnet_tags = {
-    Name = "net-${local.region}-${local.environment}-application-${local.CustomerName}"
-  }
+  # private_subnet_tags = {
+    # Name = "net-${local.region}-${local.environment}-application-${local.CustomerName}"
+  # }
 
-  database_subnet_tags = {
-    Name = "net-${local.region}-${local.environment}-data-${local.CustomerName}"
-  }
+   #database_subnet_tags = {
+     #Name = "net-${local.region}-${local.environment}-data-${local.CustomerName}"
+  # }
 
-  private_route_table_tags = {
+   #private_route_table_tags = {
     # Name = "rtb-${local.region}-${local.environment}-${local.purpose}"
-  }
+  # }
 
   tags = {
     CustomerName = "${local.CustomerName}"
